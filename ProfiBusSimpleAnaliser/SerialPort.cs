a461cbf2cae9708202264315d4581dd21193dfe5
Kamil <kamil@altel.pl>
2017-12-22 14:01:17 +0100

    First comit

---
+++ ProfiBusSimpleAnaliser/SerialPort.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Controls;
+using System.IO.Ports;
+
+using System.Threading;				// Watki Thread
+// using System.Windows.Threading;
+
+// using System.Timers;				// Timery
+
+
+namespace ProfiBusSimpleAnaliser
+{
+	delegate void rx_funct_process();
+
+	// enum STATUS { IDLE, FRAME_OK, TIMEOUT };
+
+	class SerialPortGeneric
+	{
+		protected ComboBox combobox;
+		protected SerialPort serial;
+		protected rx_funct_process rx_user_process;
+		protected System.Timers.Timer timer;
+		protected Semaphore semafor = null;
+
+		// private STATUS rx_status = STATUS.IDLE;
+
+		// rx data
+		protected Queue<byte> rx_buffer = null;		// Kolejka odbiorcza
+		// private rx_ext_funct rx_funct;
+
+		// ***************************************************************************
+		public SerialPortGeneric(ComboBox combo)
+		{
+			combobox = combo;
+			serial = new SerialPort();
+			rx_buffer = new Queue<byte>();
+
+			InitSerialComboBox();
+
+			// rx_funct = funct;
+
+			serial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(rx_handler_funct);
+
+		}	// SerialPortGeneric
+
+
+		// ***************************************************************************
+		/// Analizowanie ramki wysylanej, w generic - przezroczyste
+		protected virtual byte[] AnaliseTxFrame(byte[] buf)				// tylko metode virtual mozna przyslonic (override)
+		{
+			return buf;		// nic nie robi
+
+		}	// AnaliseTxFrame
+
+
+		// ***************************************************************************
+		/// Analizowanie wamki odebranej
+		/// @return : 1 - frame ok, 0 - error
+		protected virtual bool AnaliseRxFrame(byte[] buf)			// tylko metode virtual mozna przyslonic (override)
+		{
+			return true;
+
+		}	// AnaliseRxFrame
+
+
+		// ***************************************************************************
+		// public virtual bool ReceiveFrame(int timeout, byte[] buf, int size)
+		// ret true - frame received, false - timeout
+		public bool ReceiveFrame(int timeout, out byte [] rx_buf, out int rx_size)
+		{
+			rx_buf = null;
+			rx_size = 0;
+
+			rx_buffer.Clear();
+
+			timer = new System.Timers.Timer();
+			timer.Elapsed += new System.Timers.ElapsedEventHandler(OnTimerEvent);
+			timer.Interval = timeout;
+			timer.Enabled = true;
+
+			semafor = new Semaphore(0, 1);		// arg 1 - ile miejsce jest wolnych, arg 2 - ile jest max miejsc - (0,1) - WaitOne - czeka az bedzie realise
+			semafor.WaitOne();
+
+			// if (rx_status == STATUS.FRAME_OK)
+			if (rx_buffer.Count != 0)
+			{
+				rx_buf = rx_buffer.ToArray();
+				rx_size = rx_buffer.Count;
+				if (AnaliseRxFrame(rx_buf) == true)
+					return true;
+				else
+					return false;		// crc error
+			}
+			else
+				return false;
+
+		}	// ReceiveFrame
+
+
+		// ***************************************************************************
+		// Wewnetrzny Event odbioru Rx
+		// protected void rx_handler_funct(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+		protected virtual void rx_handler_funct(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)	// tylko metode virtual mozna przyslonic (override)
+		{
+			int rx_size = serial.BytesToRead;
+			byte[] buffer = new byte[rx_size];
+
+			int iReaded = serial.Read(buffer, 0, rx_size);
+
+			for (int i = 0; i < rx_size; i++)
+			{
+				rx_buffer.Enqueue(buffer[i]);
+			}
+
+			// rx_funct(buffer, rx_size);
+
+		}	// rx_handler_funct
+		
+
+		// ***************************************************************************
+		// Event doliczenie timera
+		void OnTimerEvent(object sender, System.Timers.ElapsedEventArgs e)
+		{
+			timer.Enabled = false;				// wylaczenie timera
+			// rx_status = STATUS.TIMEOUT;
+			semafor.Release();
+
+		}	// ReceiveFrame
+
+
+		// ***************************************************************************
+		// Funkcja wewnetrza odpalana w osobnym watku - wywoluje funkcje usera, bo nie dalo sie delegata odpalic jako watek
+		void RxSystemProcess()
+		{
+			rx_user_process();
+
+		}	// RxSystemProcess
+
+
+		// ***************************************************************************
+		// Odpalenie procesu odbierania/wysylania
+		public void TxRxStartProcess(rx_funct_process rx_usr_proc)
+		{
+			rx_user_process = rx_usr_proc;
+
+			// Thread thr = new Thread(rx_usr_proc);		// To nie chce sie kompilowac
+			Thread thd = new Thread(RxSystemProcess);
+			thd.Start();
+
+		}	// TxRxStartProcess
+
+
+		// ***************************************************************************
+		public void SendFrame(byte[] buf)	// bool wait)
+		{
+			buf = AnaliseTxFrame(buf);
+			serial.Write(buf, 0, buf.Length);
+			// int timer = 0;
+
+			/*
+			// To nie dziala
+			while (serial.BytesToWrite != 0)
+			{
+				++timer;
+				Thread.Sleep(1);
+			}
+			 * */
+
+		}	// SendFrame
+
+
+		// ***************************************************************************
+		// get, set
+		public int BaudRate
+		{
+			get
+			{
+				return serial.BaudRate;
+			}
+
+			set
+			{
+				serial.BaudRate = value;
+			}
+		}
+
+		// ***************************************************************************
+		// Wsadzenie istniejacych portow do comboboxa
+		protected void InitSerialComboBox()
+		{
+			string [] portNames = SerialPort.GetPortNames();
+
+			combobox.Items.Clear();
+
+			for (int i = 0; i < portNames.Length; i++)
+				combobox.Items.Add(portNames[i]);
+
+		}	// InitSerialPorts
+
+
+		// ***************************************************************************
+		// Przygotowuje port do odpalenia ret false - problem
+		public bool PreparePort()
+		{
+			if (serial.IsOpen)
+				serial.Close();
+
+			if (combobox.SelectedIndex != -1)
+			{
+				serial.PortName = (string)combobox.Items[combobox.SelectedIndex];
+				serial.Open();
+
+				return true;
+			}
+			else
+				return false;		// not selected
+
+		}	// GetComboboxPos
+
+
+		/*
+
+		Wojtek uzywal
+		IsOpen
+		Close
+		Write(buffer, 0, length);
+		 
+			int iReadedBytes = serialPort1.BytesToRead;
+		int iReaded = serialPort1.Read(buffer, 0, iReadedBytes);
+		
+		BaudRate = 19200;
+		DataReceived
+		 
+		 
+		 */
+
+	}
+}
---
+++ ProfiBusSimpleAnaliser/Window1.xaml.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Collections.ObjectModel;
+using System.Threading;				// Watki Thread
+using System.Windows.Threading;
+
+
+namespace ProfiBusSimpleAnaliser
+{
+
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		ProfiBus KamSerial;
+
+		int BAUDRATE = 230400;		// 250000 orig
+
+		UInt16 CRC_ERROR_POS = 0;
+		UInt16 FRAMING_ERROR_POS = 1;
+		UInt16 UNDEFINED_ERROR_POS = 2;
+		UInt16 EXCEPTION_POS = 3;
+
+
+		public ObservableCollection<Ramka_ListItem> ListaRamek = new ObservableCollection<Ramka_ListItem>();
+		// public ObservableCollection<Ramka_ListItem> ListaRamek;		// {get; set;}	<-	widze ze tego nie musi byc
+
+
+		// ***************************************************************
+		/// Dodaje do listy wpis - blad
+		void AddErrItemToList(string name)
+		{
+			Ramka_ListItem item = new Ramka_ListItem();
+			item.info = name;
+			item.odbiorca = 0;
+			item.nadawca = 0;
+			item.typ = 0;
+			item.ilosc = 0;
+
+			ListaRamek.Add(item);
+
+		}	// AddErrItemToList
+
+
+		// ***************************************************************
+		private void ResetFrameList()
+		{
+			if (ListaRamek.Count != 0)
+				ListaRamek.Clear();
+
+			AddErrItemToList("Crc Error");
+			AddErrItemToList("Framing Error");
+			AddErrItemToList("Undefined Error");
+			AddErrItemToList("Exception");
+
+		}	// ResetFrameList
+
+
+		// ***************************************************************
+		public Window1()
+		{
+			// ListaRamek = new ObservableCollection<Ramka_ListItem>();
+
+			InitializeComponent();
+			AppInit();
+
+		}	// Window1
+
+
+		// ***************************************************************
+		public void AppInit()
+		{
+			// KamSerial = new SerialPortGeneric(comboBoxPorts);
+			KamSerial = new ProfiBus(comboBoxPorts);
+
+			KamSerial.ProfiFrameOk += new delegate_FrameOk(KamSerial_ProfiFrameOk);
+			KamSerial.ProfiFrameErr += new delegate_FrameErr(KamSerial_ProfiFrameErr);
+
+			listView1.ItemsSource = ListaRamek;
+
+			ResetFrameList();
+
+		}	// AppInit
+
+
+		// ***************************************************************
+		/// Zaznaczenie checkboxa
+		private void EnableTransmisionCheckbox_Checked(object sender, RoutedEventArgs e)
+		{
+			if (EnableTransmisionCheckbox.IsChecked.Value == true)
+			{
+				if (comboBoxPorts.SelectedIndex != -1)
+				{
+					KamSerial.PreparePort();
+					KamSerial.BaudRate = BAUDRATE;
+					
+					// KamSerial.TxRxStartProcess(ReadParamsProcess);
+				}
+				else
+				{
+					MessageBox.Show("Wybierz port szeregowy", "Informacja");
+					EnableTransmisionCheckbox.IsChecked = false;
+				}
+			}
+			else
+			{
+
+			}
+
+		}	// AppInit
+
+
+		// ***************************************************************
+		// dodanie zdarznie na liscie ze wystapil wyjatek
+		void AddFrameException()
+		{
+			this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate()
+			{
+				ListaRamek[EXCEPTION_POS].ilosc++;
+				listView1.Items.Refresh();
+			});
+		
+		}	// AddFrameException
+
+
+		// ***************************************************************
+		private void TestFrameAddModife(byte odb, byte nad, byte typ)
+		{
+			for (UInt16 i = 1; i < ListaRamek.Count; i++)
+			{
+				if (ListaRamek[i].odbiorca == odb)
+					if (ListaRamek[i].nadawca == nad)
+						if (ListaRamek[i].typ == typ)		// frame exist
+						{
+							ListaRamek[i].ilosc++;
+							listView1.Items.Refresh();
+							return;
+						}
+			}
+
+			// frame not exist
+			Ramka_ListItem item = new Ramka_ListItem();
+
+			item.info = "Ok";
+			item.odbiorca = odb;
+			item.nadawca = nad;
+			item.typ = typ;
+			item.ilosc = 1;
+
+			ListaRamek.Add(item);
+
+		}	// TestIsFrameExist
+
+
+		// ***************************************************************
+		/// Event zlapania gotowej ramki profibusa
+		private void KamSerial_ProfiFrameOk(Queue<byte> rx_data)
+		{
+			this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate()
+			{
+				try
+				{
+					TestFrameAddModife(rx_data.ElementAt(0), rx_data.ElementAt(1), rx_data.ElementAt(2));
+				}
+				catch
+				{
+					AddFrameException();
+				}
+			});
+
+			/*
+			// To tez dziala
+			this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate()
+			{
+				ListaRamek.Add(new Ramka_ListItem
+				{
+					odbiorca = rx_data.ElementAt(0),
+					nadawca = rx_data.ElementAt(1),
+					typ = rx_data.ElementAt(2),
+					ilosc = 0,
+				}
+				);
+			});
+			*/
+
+		}	// KamSerial_ProfiFrameOk
+
+
+		// ***************************************************************
+		/// Event bledu ramki
+		private void KamSerial_ProfiFrameErr(RX_RET_STATE err)
+		{
+
+			this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate()
+			{
+				if (err == RX_RET_STATE.CRC_ERROR)
+				{
+					ListaRamek[CRC_ERROR_POS].ilosc++;
+				}
+				else if (err == RX_RET_STATE.FRAMING_ERROR)
+				{
+					ListaRamek[FRAMING_ERROR_POS].ilosc++;
+				}
+				else
+					ListaRamek[UNDEFINED_ERROR_POS].ilosc++;
+
+				listView1.Items.Refresh();
+			});
+
+
+		}	// KamSerial_ProfiFrameErr
+
+
+		// ***************************************************************
+		// Kasowanie listy ramek
+		private void button_clear_Click(object sender, RoutedEventArgs e)
+		{
+			ResetFrameList();
+
+		}	// KamSerial_ProfiFrameErr
+
+
+	}
+
+
+	// ***************************************************************
+	// klasa pozycji na liscie ramek
+	public class Ramka_ListItem
+	{
+		public string info { get; set; }
+		public byte odbiorca { get; set; }		// zeby dzialalo koniecznie musi byc get i set
+		public byte nadawca { get; set; }
+		public byte typ { get; set; }
+		public uint ilosc { get; set; }
+	
+	}	// Ramka_ListItem
+
+
+}
